20b492cf10fd51a27f5188c6f7fae68fcc6e4782
randomusert <127417963+randomusert@users.noreply.github.com>
2023-10-15 13:13:19 +0300

    added winmain function

--- mtd/main.c
+++ mtd/main.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 
+
+#pragma warning(push, 0)
 #include <Windows.h>
+#pragma warning(pop)
+
+
+int _cdecl WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int nCmdshow)
+{
+    return(0);
+}
